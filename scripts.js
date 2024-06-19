d3e1cb33ea894942b83b858047df6ee5417d9a3d
maestrouz <tsrichpubgm@gmail.com>
2024-06-19 13:08:48 +0500

    full

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const html = document.querySelector('html')
+const checkbox = document.getElementById('checkbox-button')
+
+checkbox.addEventListener('change', function() {
+    html.classList.toggle('darker-mode')
+})
+
+
